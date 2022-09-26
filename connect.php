9d563933cea2757c28b7cbe2a03eac690ab3034f
setyawannn <prayogasetyawan.adi@gmail.com>
2022-09-26 14:18:45 +0700

    CRUD complete

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'ukl_backup_laundry');
+$conn = mysqli_connect('localhost', 'root', '', 'backup_laundry_ukl');
 /* check connection */
 if (mysqli_connect_errno()) {
   printf("Connect failed: %s\n", mysqli_connect_error());
---
+++ member/proses_tambah_member.php
@@ -0,0 +1,23 @@
+<?php
+if ($_POST) {
+  $nama = $_POST['nama'];
+  $alamat = $_POST['alamat'];
+  $jenis_kelamin = $_POST['jenis_kelamin'];
+  $tlp = $_POST['tlp'];
+
+  if (empty($nama)) {
+    echo "<script>alert('nama member tidak boleh kosong');location.href='tambah_member.php';</script>";
+  } elseif (empty($alamat)) {
+    echo "<script>alert('alamat tidak boleh kosong');location.href='tambah_member.php';</script>";
+  } elseif (empty($tlp)) {
+    echo "<script>alert('nomor telepon tidak boleh kosong');location.href='tambah_member.php';</script>";
+  } else {
+    include "connect.php";
+    $insert = mysqli_query($conn, "insert into member (nama, alamat, jenis_kelamin, tlp ) value ('" . $nama . "','" . $alamat . "','" . $jenis_kelamin . "', '" . $tlp . "')") or die(mysqli_error($conn));
+    if ($insert) {
+      echo "<script>alert('Sukses menambahkan member');location.href='tambah_member.php';</script>";
+    } else {
+      echo "<script>alert('Gagal menambahkan member');location.href='tambah_member.php';</script>";
+    }
+  }
+}
